9ac9617cedce120ddcd914812e3083af96079f97
PossibilityJoshuaB <116594334+PossibilityJoshuaB@users.noreply.github.com>
2024-05-28 18:19:25 -0400

    Basic drive command

--- src/main/java/frc/robot/subsystems/Drive/Swerve.java
+++ src/main/java/frc/robot/subsystems/Drive/Swerve.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems.Drive;
 
 import java.io.File;
+import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.sim.ChassisReference;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OperatorConstants.AutonConstants;
 import swervelib.SwerveDrive;
@@ -16,6 +19,7 @@ import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 
@@ -68,6 +72,45 @@ public class Swerve extends SubsystemBase{
         this // Reference to this subsystem to set requirements
                                   );
     }
+    //use for pathfinding during auto
+    public Command driveToPose(Pose2d pose) {
+        PathConstraints constraints = new PathConstraints(swerveDrive.getMaximumVelocity(), 
+                                    4.0, swerveDrive.getMaximumAngularVelocity(), 
+                                    Units.degreesToRadians(720));
+        
+        return AutoBuilder.pathfindToPose(pose, constraints, 0.0);
+    }
+
+
+    //Drive command where rotational input determines robot heading
+    // public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, 
+    //                             DoubleSupplier rotationX, DoubleSupplier rotationY){
+    //     swerveDrive.setHeadingCorrection(true);
+    //     return run(()-> {
+    //         double xInput = Math.pow(translationX.getAsDouble(), 3);
+    //         double yInput = Math.pow(translationY.getAsDouble(), 3);
+    //         driveFieldOriented(swerveDrive.swerveController.getTargetSpeeds(xInput, yInput, 
+    //                                                         rotationX.getAsDouble(), 
+    //                                                         rotationY.getAsDouble(), 
+    //                                                         swerveDrive.getOdometryHeading().getRadians(),
+    //                                                         swerveDrive.getMaximumVelocity()));
+    //     });
+    // }
+
+    //drive command that uses rotational input as speed of rotation
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, 
+                                DoubleSupplier rotationX){
+        return run(() -> {
+            swerveDrive.drive(new Translation2d(Math.pow(translationX.getAsDouble(), 3) * swerveDrive.getMaximumVelocity(),
+                                Math.pow(translationY.getAsDouble(), 3) * swerveDrive.getMaximumVelocity()),
+                                Math.pow(rotationX.getAsDouble(), 3) * swerveDrive.getMaximumAngularVelocity(),
+                                true, false);
+        });
+    }
+    public void driveFieldOriented(ChassisSpeeds velocity){
+        swerveDrive.driveFieldOriented(velocity);
+    }
+
 
     public Pose2d getPose() {
         return swerveDrive.getPose();
